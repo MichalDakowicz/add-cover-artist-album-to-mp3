bc2f11c180e5ba49ec8336ff1e749eef023153d2
Michu <michal.dakowicz@infotech.edu.pl>
2024-04-27 20:10:01 +0200

    added title tags and fixed saving cover art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, APIC, TPE1, TALB, error
+from mutagen.id3 import ID3, APIC, TPE1, TALB, TIT2, error
 import os
 
 def get_mp3_files(directory):
@@ -16,6 +16,7 @@ def main(directory, album, cover_artist):
     for mp3_file in mp3_files:
         audio = MP3(mp3_file, ID3=ID3)
 
+        # adding ID3 tag if it is not present
         try:
             audio.add_tags()
         except error:
@@ -24,8 +25,9 @@ def main(directory, album, cover_artist):
         audio.tags.add(APIC(mime='image/png', type=3, desc=u'Cover', data=open(album_cover, 'rb').read()))
         audio.tags.add(TPE1(encoding=3, text=cover_artist))
         audio.tags.add(TALB(encoding=3, text=album))
-        audio.save()
-        print("Added album cover, artist, and album to " + mp3_file)
+        audio.tags.add(TIT2(encoding=3, text=os.path.basename(mp3_file)[:-4]))  # Add song title
+        audio.save(v2_version=3)
+        print("Added album cover, artist, album, and title to " + mp3_file)
 
 if __name__ == "__main__":
     directory = input("Enter the directory of the mp3 files (it will be the album name too): ")
